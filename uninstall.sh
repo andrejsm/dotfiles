42817b725fcbb8984400ce2388b7f2dc0fdf536d
Andrejs M <andrejs.m@orangelv.com>
2017-08-07 09:51:23 +0300

    Update uninstall to properly cleanup symlinks

--- uninstall.sh
+++ uninstall.sh
@@ -5,8 +5,8 @@ if [[ $REPLY =~ ^[Yy]$ ]]
 then
   # Remove theme links
   echo "Cleaning prompt theme: Pure"
-  rm -f "$HOME/dotfiles/zsh/plugins/pure/prompt_pure_setup"
-  rm -f "$HOME/dotfiles/zsh/plugins/pure/async"
+  rm -f "$HOME/dotfiles/zsh/symlinks/prompt_pure_setup"
+  rm -f "$HOME/dotfiles/zsh/symlinks/async"
 
   # Unlink .zshrc
   echo "Unlinking .zshrc"
@@ -16,7 +16,7 @@ then
   echo "Unlinking .vscode"
   rm -f "$HOME/.vscode"
   rm -f "$HOME/Library/Application Support/Code/User"
-  
+
   # Done
   echo "Done"
 fi
