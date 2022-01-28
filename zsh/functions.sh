7a1d58f0942cff9f79e5c6e4ec2e073b05a1061c
Andrejs M <andrejs.m@orangelv.com>
2022-01-28 15:26:26 +0200

    Add asdfplugs for installing required plugins

--- zsh/functions.sh
+++ zsh/functions.sh
@@ -1,6 +1,14 @@
 # Make a dir and cd in
 mkcd() { mkdir -p $1 && cd $1; }
 
+# Install required ASDF plugins
+asdfplugs() {
+  cut -d' ' -f1 .tool-versions | sort \
+    | comm -23 - <(asdf plugin-list | sort) \
+    | join -a1 - <(asdf plugin list all) \
+    | xargs -t -L1 asdf plugin add
+}
+
 # Open editor
 e() {
     if [[ $# = 0 ]]
