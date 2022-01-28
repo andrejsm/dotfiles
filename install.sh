5d3f6f201e5e2328c18ccdd05e7a372ca3c940d8
Andrejs M <andrejs.m@orangelv.com>
2022-01-28 15:33:38 +0200

    Update installation script

--- install.sh
+++ install.sh
@@ -3,7 +3,12 @@
 INSTALLER_PATH="$HOME/dotfiles"
 ZSH_PATH="$INSTALLER_PATH/zsh"
 PURE_PATH="ZSH_PATH/plugins/pure"
-VSCODE_PATH="$INSTALLER_PATH/.vscode"
+
+echo "Installing brew"
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+echo "Installing asdf"
+brew install asdf
 
 
 # Prepare prompt theme
@@ -12,15 +17,9 @@ ln -s "$ZSH_PATH/plugins/pure/pure.zsh" "$ZSH_PATH/symlinks/prompt_pure_setup"
 ln -s "$ZSH_PATH/plugins/pure/async.zsh" "$ZSH_PATH/symlinks/async"
 
 
-# Link .zshrc
 echo "Linking .zshrc"
-ln -s "$ZSH_PATH/.zshrc" "$HOME"
+echo -e "$ZSH_PATH/.zshrc\n\n$(<~/.zshrc)" > ~/.zshrc
 
-
-# Link .vscode
-echo "Linking .vscode"
-ln -s "$VSCODE_PATH" "$HOME"
-ln -s "$VSCODE_PATH/User" "$HOME/Library/Application Support/Code"
 
 # Installation is done
 echo "Done"
