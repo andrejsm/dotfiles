8c0139b8ac1d3e432968dcdaace90e024bd1f21b
Andrejs M <andrejs.m@olv.global>
2022-01-31 14:57:54 +0200

    Update install script
    now add source call to ~/.zshrc instead of requiring to create
    .zshrc_local

--- install.sh
+++ install.sh
@@ -1,24 +1,30 @@
 #!/usr/bin/env bash
 
-INSTALLER_PATH="$HOME/dotfiles"
-ZSH_PATH="$INSTALLER_PATH/zsh"
+DOTFILES_PATH="$HOME/dotfiles"
+ZSH_PATH="$DOTFILES_PATH/zsh"
 PURE_PATH="ZSH_PATH/plugins/pure"
 
 echo "Installing brew"
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+which brew >> /dev/null || /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-echo "Installing asdf"
-brew install asdf
+echo "Installing brew bundle"
+/opt/homebrew/bin/brew bundle
 
 
 # Prepare prompt theme
 echo "Preparing prompt theme: Pure"
-ln -s "$ZSH_PATH/plugins/pure/pure.zsh" "$ZSH_PATH/symlinks/prompt_pure_setup"
-ln -s "$ZSH_PATH/plugins/pure/async.zsh" "$ZSH_PATH/symlinks/async"
+ln -s "$ZSH_PATH/plugins/pure/pure.zsh" "$ZSH_PATH/symlinks/prompt_pure_setup" 2>/dev/null
+ln -s "$ZSH_PATH/plugins/pure/async.zsh" "$ZSH_PATH/symlinks/async" 2>/dev/null
 
 
+# Link .zshrc and append source call
 echo "Linking .zshrc"
-echo -e "$ZSH_PATH/.zshrc\n\n$(<~/.zshrc)" > ~/.zshrc
+ln -s "$ZSH_PATH/.zshrc" "$DOTFILES_PATH/.zshrc" 2>/dev/null
+
+LINE="if [ -f '$DOTFILES_PATH/.zshrc' ]; then source '$DOTFILES_PATH/.zshrc'; fi"
+FILE="$HOME/.zshrc"
+
+grep -qF -- "$LINE" "$FILE" || echo "\n$LINE" >> "$FILE"
 
 
 # Installation is done
